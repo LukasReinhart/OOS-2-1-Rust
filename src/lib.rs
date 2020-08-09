4b6e34f6bdd79980e59641bf42a9f636576ed383
LukasReinhartGraf <if19b019@technikum-wien.at>
2020-08-09 11:13:47 +0200

    WorldPosition, AI rework - 115 min

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,67 @@
 use std::sync::Mutex;
 use rand;
+use rand::Rng;
+
+pub struct WorldPosition {
+    x: usize,
+    y: usize,
+    map_width: usize,
+    map_height: usize,
+}
+
+impl WorldPosition {
+    pub fn new(map: &WorldMap, x: usize, y: usize) -> Self {
+        WorldPosition {
+            x,
+            y,
+            map_width: map.width(),
+            map_height: map.height(),
+        }
+    }
+
+    pub fn x(&self) -> usize {
+        self.x
+    }
+    pub fn y(&self) -> usize {
+        self.y
+    }
+    pub fn set_x(&mut self, x: usize) {
+        if x < self.map_width {
+            self.x = x;
+        } else {
+            self.x = self.map_width;
+        }
+    }
+    pub fn set_y(&mut self, y: usize) {
+        if y < self.map_width {
+            self.y = y;
+        } else {
+            self.y = self.map_width;
+        }
+    }
+
+    /// Translates X/Y coordinates to an index in the 'fields' vector.
+    pub fn to_index(&self) -> usize {
+        self.x + self.y * self.map_width
+    }
+
+    // Teleports the robot to a random position (within world map bounds).
+    pub fn randomize(&mut self) {
+        self.x = rand::random::<usize>() % self.map_width;
+        self.y = rand::random::<usize>() % self.map_height;
+    }
+}
+
+impl Clone for WorldPosition {
+    fn clone(&self) -> Self {
+        Self {
+            x: self.x,
+            y: self.y,
+            map_width: self.map_width,
+            map_height: self.map_height,
+        }
+    }
+}
 
 pub struct WorldMap {
     width: usize,
@@ -23,11 +85,6 @@ impl WorldMap {
         }
     }
 
-    /// Translates X/Y coordinates to an index in the 'fields' vector.
-    fn position_index(&self, x: usize, y: usize) -> usize {
-        x + y * self.width
-    }
-
     /// Sets all fields to a random value between (including) 1 to 'max_field_Score' each.
     pub fn randomize_fields(&self, max_field_score: usize) {
         if let Ok(fields) = self.fields.lock() {
@@ -35,8 +92,9 @@ impl WorldMap {
             if let Ok(points_left) = self.points_left.lock() {
                 let mut points_left = points_left;
 
+                let mut rng = rand::thread_rng();
                 for i in 0..fields.len() {
-                    let new_points = (rand::random::<usize>() % max_field_score) + 1;
+                    let new_points = (rng.gen::<usize>() % max_field_score) + 1;
                     *points_left += new_points - fields[i];
                     fields[i] = new_points;
                 }
@@ -45,12 +103,12 @@ impl WorldMap {
     }
 
     /// Lowers score at the given coordinates by 1, to a minimum of 0, and returns 1 if successful.
-    pub fn deduct_score_at(&self, x: usize, y: usize) -> usize {
+    pub fn deduct_score_at(&self, pos: &WorldPosition) -> usize {
         let mut harvested = 0;
 
         if let Ok(fields) = self.fields.lock() {
             let mut fields = fields;
-            let idx = self.position_index(x, y);
+            let idx = pos.to_index();
             if fields[idx] > 0 {
                 if let Ok(points_left) = self.points_left.lock() {
                     let mut points_left = points_left;
@@ -65,9 +123,9 @@ impl WorldMap {
         harvested
     }
 
-    pub fn points_at(&self, x: usize, y: usize) -> usize {
+    pub fn points_at(&self, pos: &WorldPosition) -> usize {
         if let Ok(fields) = self.fields.lock() {
-            let idx = self.position_index(x, y);
+            let idx = pos.to_index();
             fields[idx]
         } else {
             0
@@ -108,6 +166,7 @@ mod tests {
     fn worldmap_basic_deduction() {
         let map = WorldMap::new(5, 5);
         map.randomize_fields(5);
-        assert_eq!(map.deduct_score_at(2, 2), 1);
+        let pos = WorldPosition::new(&map, 2, 2);
+        assert_eq!(map.deduct_score_at(&pos), 1);
     }
 }
--- src/robots.rs
+++ src/robots.rs
@@ -3,15 +3,14 @@ use std::fmt;
 use std::thread;
 use std::ops::{Deref, DerefMut};
 
+use crate::WorldPosition;
 use crate::WorldMap;
 
 pub struct RobotCore {
     id: usize,
-
     score: usize,
+    pos: WorldPosition,
     map: Arc<WorldMap>,
-    x: usize,
-    y: usize,
 }
 
 impl RobotCore {
@@ -19,9 +18,8 @@ impl RobotCore {
         Self {
             id,
             score: 0,
+            pos: WorldPosition::new(&map, 0, 0),
             map,
-            x: 0,
-            y: 0,
         }
     }
 
@@ -30,18 +28,11 @@ impl RobotCore {
         self.score
     }
 
-    // Teleports the robot to a random position (within world map bounds).
-    pub fn randomize_position(&mut self) {
-        self.x = rand::random::<usize>() % self.map.width();
-        self.y = rand::random::<usize>() % self.map.height();
-    }
-
     /// Programs and unleashes the robot, sending it on an uncontrollable rampage through its world map until all score is gone.
     pub fn run(&mut self, step_fn: fn(&mut Self)) {
         while self.map.points_left() > 0 {
             step_fn(self);
             
-            self.score += self.map.deduct_score_at(self.x, self.y);
             // give others a chance?
             thread::yield_now();
         }
@@ -79,32 +70,19 @@ impl RandomBot {
         )
     }
 
-    /// Step in random (within world map bounds) direction.
-    fn step_fn(robot: &mut RobotCore) {
-        let dir = rand::random::<u8>() % 4;
-
-        for i in 0..4 {
-            let dir = (dir + i) % 4;
-
-            if dir == 0 && robot.x > 0 {
-                robot.x -= 1;
-                return;
-            }
-            else if dir == 1 && robot.y > 0 {
-                robot.y -= 1;
-                return;
-            }
-            else if dir == 2 && robot.x < robot.map.width() - 1 {
-                robot.x += 1;
-                return;
-            }
-            else if dir == 3 && robot.y < robot.map.height() - 1 {
-                robot.y += 1;
-                return;
-            }
-        }
+    pub fn randomize_position(&mut self) {
+        self.pos.randomize();
     }
 
+    /// Step in random (within world map bounds) direction.
+    fn step_fn(robot: &mut RobotCore) {
+        if let Some(new_pos) = try_step(&robot.pos, &robot.map, false) {
+            robot.pos = new_pos;
+        }
+
+        robot.score += robot.map.deduct_score_at(&robot.pos);
+    }
+    
     pub fn step(&mut self) {
         Self::step_fn(&mut self.0);
     }
@@ -145,53 +123,22 @@ impl NearsightBot {
         )
     }
 
+    pub fn randomize_position(&mut self) {
+        self.pos.randomize();
+    }
+
     /// Step in random (within world map bounds) direction.
     fn step_fn(robot: &mut RobotCore) {
-        let dir = rand::random::<u8>() % 4;
-
         // Go to score if possible
-        for i in 0..4 {
-            let dir = (dir + i) % 4;
-
-            if dir == 0 && robot.x > 0 && robot.map.points_at(robot.x-1, robot.y) > 0 {
-                robot.x -= 1;
-                return;
-            }
-            else if dir == 1 && robot.y > 0 && robot.map.points_at(robot.x, robot.y-1) > 0 {
-                robot.y -= 1;
-                return;
-            }
-            else if dir == 2 && robot.x < robot.map.width() - 1 && robot.map.points_at(robot.x+1, robot.y) > 0 {
-                robot.x += 1;
-                return;
-            }
-            else if dir == 3 && robot.y < robot.map.height() - 1 && robot.map.points_at(robot.x, robot.y+1) > 0 {
-                robot.y += 1;
-                return;
-            }
+        if let Some(new_pos) = try_step(&robot.pos, &robot.map, true) {
+            robot.pos = new_pos;
         }
-
         // Go elsewhere to find score farther away
-        for i in 0..4 {
-            let dir = (dir + i) % 4;
-
-            if dir == 0 && robot.x > 0 {
-                robot.x -= 1;
-                return;
-            }
-            else if dir == 1 && robot.y > 0 {
-                robot.y -= 1;
-                return;
-            }
-            else if dir == 2 && robot.x < robot.map.width() - 1 {
-                robot.x += 1;
-                return;
-            }
-            else if dir == 3 && robot.y < robot.map.height() - 1 {
-                robot.y += 1;
-                return;
-            }
+        else if let Some(new_pos) = try_step(&robot.pos, &robot.map, false) {
+            robot.pos = new_pos;
         }
+
+        robot.score += robot.map.deduct_score_at(&robot.pos);
     }
 
     pub fn step(&mut self) {
@@ -205,6 +152,61 @@ impl NearsightBot {
 }
 
 
+fn try_step(current_pos: &WorldPosition, map: &WorldMap, check_score: bool) -> Option<WorldPosition> {
+    let dir = rand::random::<u8>() % 4;
+
+    for i in 0..4 {
+        let dir = (dir + i) % 4;
+
+        //TODO elegant way to move the "in-bounds" check to the WorldPosition implementation?
+        match dir {
+            0 => {
+                let x = current_pos.x();
+                if x > 0 {
+                    let mut new_pos = current_pos.clone();
+                    new_pos.set_x( new_pos.x() - 1 );
+                    if !check_score || map.points_at(&new_pos) > 0 {
+                        return Some(new_pos);
+                    }
+                }
+            },
+            1 => {
+                let y = current_pos.y();
+                if y > 0 {
+                    let mut new_pos = current_pos.clone();
+                    new_pos.set_y( new_pos.y() - 1 );
+                    if !check_score || map.points_at(&new_pos) > 0 {
+                        return Some(new_pos);
+                    }
+                }
+            },
+            2 => {
+                let x = current_pos.x();
+                if x < map.width() - 1 {
+                    let mut new_pos = current_pos.clone();
+                    new_pos.set_x( new_pos.x() + 1 );
+                    if !check_score || map.points_at(&new_pos) > 0 {
+                        return Some(new_pos);
+                    }
+                }
+            },
+            _ => {
+                let y = current_pos.y();
+                if y < map.height() - 1 {
+                    let mut new_pos = current_pos.clone();
+                    new_pos.set_y( new_pos.y() + 1 );
+                    if !check_score || map.points_at(&new_pos) > 0 {
+                        return Some(new_pos);
+                    }
+                }
+            },
+        }
+    }
+    None
+}
+
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -215,12 +217,12 @@ mod tests {
         let mut new_robot = RandomBot::new(0, Arc::clone(&map));
 
         new_robot.step();
-        assert_eq!(new_robot.x, 0);
-        assert_eq!(new_robot.y, 1);
+        assert_eq!(new_robot.pos.x(), 0);
+        assert_eq!(new_robot.pos.y(), 1);
 
         new_robot.step();
-        assert_eq!(new_robot.x, 0);
-        assert_eq!(new_robot.y, 0);
+        assert_eq!(new_robot.pos.x(), 0);
+        assert_eq!(new_robot.pos.y(), 0);
     }
 
     #[test]
@@ -229,12 +231,12 @@ mod tests {
         let mut new_robot = RandomBot::new(0, Arc::clone(&map));
 
         new_robot.step();
-        assert_eq!(new_robot.x, 1);
-        assert_eq!(new_robot.y, 0);
+        assert_eq!(new_robot.pos.x(), 1);
+        assert_eq!(new_robot.pos.y(), 0);
 
         new_robot.step();
-        assert_eq!(new_robot.x, 0);
-        assert_eq!(new_robot.y, 0);
+        assert_eq!(new_robot.pos.x(), 0);
+        assert_eq!(new_robot.pos.y(), 0);
     }
 
     #[test]
@@ -243,12 +245,12 @@ mod tests {
         let mut new_robot = NearsightBot::new(0, Arc::clone(&map));
 
         new_robot.step();
-        assert_eq!(new_robot.x, 0);
-        assert_eq!(new_robot.y, 1);
+        assert_eq!(new_robot.pos.x(), 0);
+        assert_eq!(new_robot.pos.y(), 1);
 
         new_robot.step();
-        assert_eq!(new_robot.x, 0);
-        assert_eq!(new_robot.y, 0);
+        assert_eq!(new_robot.pos.x(), 0);
+        assert_eq!(new_robot.pos.y(), 0);
     }
 
     #[test]
@@ -257,11 +259,11 @@ mod tests {
         let mut new_robot = NearsightBot::new(0, Arc::clone(&map));
 
         new_robot.step();
-        assert_eq!(new_robot.x, 1);
-        assert_eq!(new_robot.y, 0);
+        assert_eq!(new_robot.pos.x(), 1);
+        assert_eq!(new_robot.pos.y(), 0);
 
         new_robot.step();
-        assert_eq!(new_robot.x, 0);
-        assert_eq!(new_robot.y, 0);
+        assert_eq!(new_robot.pos.x(), 0);
+        assert_eq!(new_robot.pos.y(), 0);
     }
 }
